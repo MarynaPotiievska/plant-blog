0ddb77971a80dd3df1743a11d4ed6889b84b98d2
Maryna Potiievska <marynochka93@gmail.com>
2025-09-07 17:48:50 +0300

    add footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,28 @@
 import styles from "./Footer.module.css";
 
+const footerLinks = [
+  { text: "Terms of Service", href: "/" },
+  { text: "Privacy Policy", href: "/" },
+  { text: "Subscription Policy", href: "/" },
+];
+
 export function Footer() {
- 
+  return (
+    <footer className={styles.footer}>
+      <div className="container">
+        <ul className={styles.list}>
+          {footerLinks.map((link) => (
+            <li key={link.text} className={styles.item}>
+              <a href={link.href} className={styles.link}>
+                {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <p className={styles.text}>
+          ©Copyright © 2020 PlantIn. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
